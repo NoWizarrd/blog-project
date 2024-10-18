d0a209ac791c4f2abf1bb8ab28203ae4a2c41735
Aleksey <pnev.leha@mail.ru>
2024-10-18 23:55:08 +0700

    add i18n

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,6 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import classes from "./Navbar.module.scss";
-import { Link } from "react-router-dom";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,7 +1,8 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import classes from './Sidebar.module.scss'
 import { useState } from 'react';
-import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher';
+import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { LangSwitcher } from 'widgets/LangSwitcher/LangSwitcher';
 
 
 interface SidebarProps {
@@ -21,6 +22,7 @@ export const Sidebar = ({className}: SidebarProps) => {
           <button onClick={onToggle}>toggle</button>
         <div className={classes.switchers}>
             <ThemeSwitcher/>
+            <LangSwitcher className={classes.lang}/>
         </div>
       </div>
     );
