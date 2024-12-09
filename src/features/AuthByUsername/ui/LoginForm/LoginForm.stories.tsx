7f340369bcaa0e189b164b9f68def02e2f2869fd
Aleksey <pnev.leha@mail.ru>
2024-12-09 18:58:49 +0700

    test

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -10,7 +10,7 @@ export default {
         backgroundColor: { control: 'color' },
     },
 } as ComponentMeta<typeof LoginForm>;
-
+// adasdsad
 const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
 
 export const Primary = Template.bind({});
